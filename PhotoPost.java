405f6b54a6663e56c7d5a28b344dc6f9b413e1d3
Leo Tan <leotan213@gmail.com>
2019-02-13 19:27:57 -0500

    Added test cases

--- PhotoPost.java
+++ PhotoPost.java
@@ -11,17 +11,69 @@ public class PhotoPost extends Post {
     }
 
     public String getCaption() {
-	     return caption;
-    }
+      return caption;
+  }
 
-    public String getFileName() {
-	     return fileName;
-    }
+  public String getFileName() {
+      return fileName;
+  }
 
-    public String toString() {
-    	String str = new String();
-    	str = super.toString() + ", " + fileName + ", " + caption;
-    	return str;
+  public String toString() {
+   String str = new String();
+   str = super.toString() + ", " + fileName + ", " + caption;
+   return str;
+}
+
+/*public static void testConstructor() {
+
+    System.out.println();
+    System.out.println("testConstructor");
+
+    PhotoPost myPost = new PhotoPost("username", "hello.jpg", "hello world");
+
+    System.out.println("username " + myPost.getUserName());
+    System.out.println("hello world " + myPost.getCaption());
+    System.out.println("hello.jpg " + myPost.getFileName());
+
+}
+
+public static void testNotPopularPhotoPost() {
+
+    System.out.println();
+    System.out.println("testNotPopularPhotoPost");
+
+    PhotoPost myPost = new PhotoPost("username", "hello.jpg", "hello world");
+    System.out.println(myPost.isPopular());
+
+    for (int i = 0; i < 99; i++) {
+        myPost.like();
     }
+    System.out.println(myPost.isPopular());
+
+}
+
+public static void testPopularTextPost() {
+
+    System.out.println();
+    System.out.println("testPopularTextPost");
+
+    PhotoPost myPost = new PhotoPost("username", "hello.jpg", "hello world");
+
+    for (int i = 0; i < 101; i++) {
+        myPost.like();
+    }
+    System.out.println(myPost.isPopular());
+    myPost.like();
+    System.out.println(myPost.isPopular());
+
+}
+
+public static void main(String[] args) {
+
+    testConstructor();
+    testNotPopularPhotoPost();
+    testPopularTextPost();
+
+}*/
 
 }
