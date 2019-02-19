59782e81e8467104dfb1ebfe0aa06875d6693823
Leo Tan <leotan213@gmail.com>
2019-02-18 23:58:03 -0500

    Add files via upload

---
+++ AbstractSeries.java
@@ -0,0 +1,10 @@
+public abstract class AbstractSeries implements Series {
+
+    public double[] take(int k) {
+
+        // implement the method
+        double [] partialSum = new double [k];
+        return partialSum;
+    }
+
+}
